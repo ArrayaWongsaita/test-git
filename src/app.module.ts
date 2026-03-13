beb48b4dabbc1a1af8c9bfcd287c9bb95336da99
ArrayaWongsaita <Wongsaita.a@gmail.com>
2026-03-13 11:10:01 +0700

    feat(auth):create login method to controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './modules/todo/todo.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [AuthModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
